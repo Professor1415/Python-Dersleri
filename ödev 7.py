b36571b7ccc3c3b01cc7b368f0606bfedac2c37a
Mustafa <bacaksizmustafa2703@gmail.com>
2025-01-02 22:54:51 +0300

    Delete ödev 7.py

--- ödev 7.py
+++
@@ -1,9 +0,0 @@
-"""
-    module1 (db)      : urunler
-    module2 (methods) : urunEkle(), urunGuncelle(), urunleriGetir()
-    module3 (app)     :
-
-        yeni ürün ekleme => urunEkle("iphone 15", 60000)
-        ürün güncelle    => urunGuncelle(1, "iphone 15 pro", 80000)
-        ürünleri listele => urunleriGetir() 
-"""
